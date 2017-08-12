d17af5f81692db640a69ef33878dc1c970c7aad0
dao16592 <daontph04190.fpt.edu.vn>
2017-08-12 21:27:51 +0700

    fix file

---
+++ app.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Dao_FPT on 8/12/2017.
+ */
+//alert('hello');
+//document.write('hello world');
+//console.log('hello world');
+//var a=10;
+//var numA=50;
+//var b="name";
+//console.log(b);
+//var X='ban da sai';
+//console.log(X);
+//var m=null;
+//console.log(m);
+//console.log(x);
+//var arr={age:25, name:'dao'};
+//console.log(arr.age+' '+arr.name);
+//var numB = 10;
+//console.log(numB);
+//var age=25;                                  -----toan tu 3 ngoi
+//console.log(age ==32 ? 'my name is dao': 'it is not my name');
+//var numC = 80;
+//if(numC > 20 && numC<=60){
+//    console.log("you are old enough");
+//}
+//    else if(numC > 60){
+//        console.log("you are too old");
+//}
+//    else{
+//            console.log("you are too young");
+//}
+//
+//gsdfgdf
+var height=1.53;
+var weight=40;
+var body=weight/(height*height);
+if (body<18.5){
+    console.log(body);
+    console.log("thin");
+
+}
+else if (body>=18.5 && body<25){
+    console.log(body);
+    console.log("normal");
+}
+else{
+    console.log(body);
+    console.log("fat");
+}
